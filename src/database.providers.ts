9cafb4e7524bdcbccbaadcc6c62d1ff563660b16
Steve Jamerson <stevejamerson@outlook.com>
2023-09-20 12:07:18 -0300

    feat: criando migracao cpf e cnpj table

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -2,6 +2,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { CreateSchoolsTable1695176535556 } from './migrations/1695176535556-CreateSchoolsTable';
 import { CreateStudentsTable1695178505238 } from './migrations/1695178505238-CreateStudentsTable';
 import { CreateSchoolsStudentsTable1695178700211 } from './migrations/1695178700211-CreateSchoolsStudentsTable';
+import { AddCpfAndCnpjTable1695218323170 } from './migrations/1695218323170-AddCpfAndCnpjTable';
 
 const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
@@ -32,5 +33,6 @@ export const dataSource = new DataSource({
         CreateSchoolsTable1695176535556,
         CreateStudentsTable1695178505238,
         CreateSchoolsStudentsTable1695178700211,
+        AddCpfAndCnpjTable1695218323170,
     ],
 });

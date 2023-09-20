84aae327b6c9d6ee0f74a92a89434b4b50b8ae2a
Steve Jamerson <stevejamerson@outlook.com>
2023-09-20 13:21:46 -0300

    fix: corrigindo erros das validacoes

--- src/utils/functions/validations.ts
+++ src/utils/functions/validations.ts
@@ -1,62 +1,65 @@
 export function validateCPF(cpf: string): boolean {
-    // Remove all non-numeric characters from the input CPF
-    cpf = cpf.replace(/\D/g, '');
+    // Remove non-numeric characters from the CPF.
+    const numericCPF = cpf.replace(/\D/g, '');
 
-    // Check if the CPF has exactly 11 digits or consists of a sequence of repeated digits
-    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) return false;
+    // Check if the CPF has 11 digits or is a sequence of repeated numbers.
+    if (numericCPF.length !== 11 || !!numericCPF.match(/(\d)\1{10}/)) {
+        return false; // Invalid CPF.
+    }
 
-    // Function to calculate a verification digit based on a slice of the CPF
-    const calculateVerificationDigit = (sliceStart) => {
-        let sum = 0;
-        for (let i = 0; i < 9; i++) {
-            sum += parseInt(cpf.charAt(i + sliceStart)) * (10 - i);
-        }
+    // Convert the CPF into an array of numbers.
+    const cpfNumbers = numericCPF.split('').map((digit) => parseInt(digit));
 
-        // Calculate the remainder and determine the verification digit
-        const remainder = sum % 11;
-        return remainder < 2 ? 0 : 11 - remainder;
-    };
+    // Function to calculate the verification digit.
+    function calculateVerificationDigit(count) {
+        const sum = cpfNumbers
+            .slice(0, count - 12)
+            .reduce((partialSum, digit, index) => {
+                return partialSum + digit * (count - index);
+            }, 0);
+        const remainder = ((sum * 10) % 11) % 10;
+        return remainder;
+    }
 
-    // Calculate the first verification digit based on the first 9 digits of the CPF
-    const verificationDigit1 = calculateVerificationDigit(0);
+    // Check if the CPF's verification digits are valid.
+    const verificationDigit1 = calculateVerificationDigit(10);
+    const verificationDigit2 = calculateVerificationDigit(11);
 
-    // Calculate the second verification digit based on the last 9 digits of the CPF
-    const verificationDigit2 = calculateVerificationDigit(9);
-
-    // Check if the calculated verification digits match the digits in the input CPF
+    // Return true if the verification digits are valid, otherwise return false.
     return (
-        parseInt(cpf.charAt(9)) === verificationDigit1 &&
-        parseInt(cpf.charAt(10)) === verificationDigit2
+        verificationDigit1 === cpfNumbers[9] &&
+        verificationDigit2 === cpfNumbers[10]
     );
 }
 
 export function validateCNPJ(cnpj: string): boolean {
-    // Remove non-numeric characters
-    cnpj = cnpj.replace(/[^\d]/g, '');
+    // Remove all non-digit characters from the CNPJ
+    cnpj = cnpj.replace(/\D/g, '');
 
-    // Check if the CNPJ has 14 digits and if all digits are the same
-    if (cnpj.length !== 14 || /^(\d)\1+$/.test(cnpj)) return false;
+    // Check for invalid CNPJ conditions
+    if (cnpj.length !== 14 || !/^\d{14}$/.test(cnpj)) {
+        return false;
+    }
 
     // Calculate the first verification digit
     let sum = 0;
+    const weight = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
     for (let i = 0; i < 12; i++) {
-        sum += parseInt(cnpj.charAt(i)) * (13 - i);
+        sum += parseInt(cnpj.charAt(i)) * weight[i];
     }
     const firstDigit = sum % 11 < 2 ? 0 : 11 - (sum % 11);
 
-    // Verify the first verification digit
-    if (parseInt(cnpj.charAt(12)) !== firstDigit) return false;
-
     // Calculate the second verification digit
     sum = 0;
+    weight.unshift(6); // Add 6 to the beginning of the weight array
     for (let i = 0; i < 13; i++) {
-        sum += parseInt(cnpj.charAt(i)) * (14 - i);
+        sum += parseInt(cnpj.charAt(i)) * weight[i];
     }
     const secondDigit = sum % 11 < 2 ? 0 : 11 - (sum % 11);
 
-    // Verify the second verification digit
-    if (parseInt(cnpj.charAt(13)) !== secondDigit) return false;
-
-    // CNPJ is valid
-    return true;
+    // Check if both verification digits match
+    return (
+        parseInt(cnpj.charAt(12)) === firstDigit &&
+        parseInt(cnpj.charAt(13)) === secondDigit
+    );
 }

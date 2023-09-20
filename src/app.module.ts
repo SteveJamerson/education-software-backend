aeddfa976805f92bf6b816a90b90cb011310c968
Steve Jamerson <stevejamerson@outlook.com>
2023-09-20 17:34:03 -0300

    feat: criando projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChallengesModule } from './challenges/challenges.module';
+import { ProjectsModule } from './projects/projects.module';
 import { SchoolsModule } from './schools/schools.module';
 import { StudentsModule } from './students/students.module';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-    imports: [SchoolsModule, StudentsModule, TasksModule, ChallengesModule],
+    imports: [
+        SchoolsModule,
+        StudentsModule,
+        TasksModule,
+        ChallengesModule,
+        ProjectsModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })

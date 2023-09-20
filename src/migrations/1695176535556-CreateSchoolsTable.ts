81b2f70cb9f134a2f6f65ebfadfc18f95cfb39b3
Steve Jamerson <stevejamerson@outlook.com>
2023-09-19 23:53:09 -0300

    feat: criando migracao schools table

---
+++ src/migrations/1695176535556-CreateSchoolsTable.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+const tableName = 'schools';
+
+export class CreateSchoolsTable1695176535556 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: tableName,
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        isUnique: true,
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'categorie',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'CURRENT_TIMESTAMP',
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable(tableName);
+    }
+}
